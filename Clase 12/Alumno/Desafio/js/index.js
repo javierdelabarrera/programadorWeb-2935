815e5c231c086ba42f7fff63357c65dca7a6cc83
Javier <jbdelab@gmail.com>
2018-07-09 20:06:09 -0300

    Desafio Clase 12

--- Clase 12/Alumno/Desafio/js/index.js
+++ Clase 12/Alumno/Desafio/js/index.js
@@ -1,1 +1,24 @@
 console.log('Init app: Desafio Clase 12')
+
+// capturo input sobre field de email
+var inputEmailJQNode = $('#email')
+
+// invoco funcion validateEmail en evento blur de field de email
+inputEmailJQNode.blur(validateEmail)
+
+// funcion validadora de mail
+function validateEmail (event) {
+  var jqNode = $(this)
+
+  if (
+    jqNode.val() &&
+    jqNode.val().indexOf('@') !== -1 &&
+    jqNode.val().indexOf('.') !== -1
+  ) {
+    console.log('email correcto')
+    jqNode.addClass('is-valid').removeClass('is-invalid')
+  } else {
+    console.log('email incorrecto')
+    jqNode.addClass('is-invalid').removeClass('is-valid')
+  }
+}
