f1dcb7c919927c3b5f31bc35a746f1621af4853d
Javier <jbdelab@gmail.com>
2018-07-15 13:48:18 -0300

    Correccion Desafio Clase 13

--- Clase 13/Alumno/Desafío/js/index.js
+++ Clase 13/Alumno/Desafío/js/index.js
@@ -1,19 +1,11 @@
 $(document).ready(function () {
-  console.log('Init app: Desafio Clase 13 .')
+  console.log('Init app: Desafio Clase 13 Domingo II')
 
   // capturo elementos html por id y lo asigno a vars
   var firstNameInputNode = $('#firstName')
   var emailInputNode = $('#email')
   var commentsInputNode = $('#comments')
   var submitInputNode = $('#submitButton')
-
-  // flags para submitInputNode
-
-  var textFieldFlag = false
-  var emailFieldFlag = false
-
-  // boton submit inicializa disabled
-  submitInputNode.attr('disabled', true)
 
   // handler blur, en one, invoco funcion de validacion
   firstNameInputNode.one('blur', validateTextField)
@@ -48,24 +40,25 @@ $(document).ready(function () {
       // siempre remuevo primero
       inputNode.removeClass('is-invalid')
       inputNode.addClass('is-valid')
-      textFieldFlag = true
     }
 
     if (event.type === 'blur') {
       inputNode.on('input', validateTextField)
     }
 
-    console.log(value, event.type)
+    validateButton()
   }
 
   function validateEmailField (e) {
     var inputNode = $(this)
 
     var value = inputNode.val()
+    // elimino validacion de posible error anterior
     inputNode.next().remove()
 
     var errorText = ''
 
+    // cuando valido voy llenando el errorText
     if (!value) {
       errorText = 'Campo requerido'
     } else {
@@ -88,17 +81,23 @@ $(document).ready(function () {
       // siempre remuevo primero
       inputNode.removeClass('is-invalid')
       inputNode.addClass('is-valid')
-      emailFieldFlag = true
     }
 
     if (event.type === 'blur') {
       inputNode.on('input', validateEmailField)
     }
+
+    validateButton()
   }
 
-  // flags para activar submit button
-  if (textFieldFlag && emailFieldFlag) {
-    submitInputNode.attr('disabled', false)
-    console.log('habilite el boton submit')
+  function validateButton (e) {
+    // capturo class valid en un var objeto jquery
+    var validInputNodes = $('.is-valid')
+
+    if (validInputNodes.length === 3) {
+      submitInputNode.attr('disabled', false)
+    } else {
+      submitInputNode.attr('disabled', true)
+    }
   }
 })
