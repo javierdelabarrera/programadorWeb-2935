98536e69074a7ef86389884c22a5b7fab9e0b7bd
Javier <jbdelab@gmail.com>
2018-07-23 20:49:29 -0300

    Desafio Clase 14

--- Clase 14/Alumno/Desafío/js/index.js
+++ Clase 14/Alumno/Desafío/js/index.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
   console.log('Init app: Desafio Clase 14')
 
-  var tableTestNode = $('#tableBody')
+  var elementToRemove = $('button')
 
-  removeElementWithAnimation(tableTestNode)
+  elementToRemove.on('click', removeElementWithAnimation)
 
-  function removeElementWithAnimation (e) {}
+  function removeElementWithAnimation (e) {
+    $(this).parent().parent().hide(2000, function () {
+      $(this).remove()
+    })
+  }
 })
